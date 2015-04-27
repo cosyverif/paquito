e6eaf5b915b857f6127b044c11ad47e0b64dadf4
melentan <blood.kings@laposte.net>
2015-04-27 09:43:31 +0200

    Ajout lanceur de commandes

--- src/Main.php
+++ src/Main.php
@@ -4,10 +4,20 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use \Symfony\Component\Console\Application;
 use \Paquito\Command\Update;
+use \Paquito\Command\Check;
+use \Paquito\Command\Parse;
+use \Paquito\Command\Prune;
+use \Paquito\Command\Write;
+use \Paquito\Command\Normalize;
 
 // FIXME: erase when release
 error_reporting(E_ALL | E_STRICT);
 
 $application = new Application('paquito', '0.1');
 $application->add(new Update());
+$application->add(new Parse());
+$application->add(new Check());
+$application->add(new Prune());
+$application->add(new Write());
+$application->add(new Normalize());
 $application->run();
