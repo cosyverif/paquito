0d2f95a1fd7ae3be5e2f0f49a6ef51389ffaf078
melentan <blood.kings@laposte.net>
2015-07-27 15:12:17 +0200

    Improved error messages

--- src/Command/Check.php
+++ src/Command/Check.php
@@ -46,21 +46,23 @@ class Check extends Command
     }
 
 		/* Check a structure of files */
-	protected function check_files($struct) {
+	protected function check_files($fieldbase, $struct) {
 		foreach ($struct as $f_key => $f_value) {
 			/* If there is not source path (in others words the field is empty) */
 			if (empty($f_value)) {
-				$this->logger->error($this->getApplication()->translator->trans('check.void', array('%key%' => $f_key)));
+				$fieldbase = implode(' -> ', $fieldbase);
+				$this->logger->error($this->getApplication()->translator->trans('check.void', array('%key%' => $f_key, '%path%' => $fieldbase." -> $f_key")));
 
 				exit(-1);
 			}
 			/* If the file will have specifics permissions */
 			if (is_array($f_value)) {
 				/* Analysis of the file structure (when there is the permissions) */
-				$this->check_field($f_key, $f_value, array('Source', 'Permissions'), array('Source', 'Permissions'));
+				$this->check_field($fieldbase, $f_value, array('Source', 'Permissions'), array('Source', 'Permissions'));
 				/* The permissions have to be numbers (octal) */
 				if (!preg_match('/^[0-7]{3,4}$/', $f_value['Permissions'])) {
-					$this->logger->error($this->getApplication()->translator->trans('check.incorrect', array('%field%' => 'Permissions', '%value%' => $f_value['Permissions'])));
+					$fieldbase = implode(' -> ', $fieldbase);
+					$this->logger->error($this->getApplication()->translator->trans('check.incorrect', array('%field%' => $fieldbase.' -> Permissions', '%value%' => $f_value['Permissions'])));
 
 					exit(-1);
 				} else if (preg_match('/^[0-7]{4}$/', $f_value['Permissions'])) { /* If the permissions mask of a file owns a special bit, warns the user */
@@ -72,22 +74,25 @@ class Check extends Command
 	}
 
 
-    /* fieldbase: Name of the superior field
+    /* fieldbase: Path of the superiors fields
      * struct: Structure that contains the superior field
      * array_comparer: Array of standards fields of the superior field
      * array_min: Array of the fields which must appear in the superior field */
     protected function check_field($fieldbase, $struct, $array_comparer, $array_min)
-    {
+	{
         /* Total number of expected fields. This number will be decremented for each expected
          * fields found. If more than 0, error (bacause it misses one or more required fields) */
         $fieldmin = count($array_min);
-        foreach ($struct as $key => $value) {
+		$fieldbase = implode(' -> ', $fieldbase);
+		/* For each field */
+		foreach ($struct as $key => $value) {
+			/* If the name of the current field doesn't part of the superior field */
             if (!in_array($key, $array_comparer)) {
-                $this->logger->error($this->getApplication()->translator->trans('check.field', array('%value%' => $key)));
+                $this->logger->error($this->getApplication()->translator->trans('check.field', array('%value%' => $key, '%path%' => $fieldbase." -> $key")));
 
                 exit(-1);
             } elseif (empty($value)) {
-                $this->logger->error($this->getApplication()->translator->trans('check.void', array('%key%' => $key)));
+                $this->logger->error($this->getApplication()->translator->trans('check.void', array('%key%' => $key, '%path%' => $fieldbase." -> $key")));
 
                 exit(-1);
             }
@@ -123,29 +128,29 @@ class Check extends Command
         $this->logger = new ConsoleLogger($output);
 
         /* Analysis of the root structure */
-        $this->check_field('Root', $struct, $this->keys_first, $this->keys_first_min);
+        $this->check_field(array('Root'), $struct, $this->keys_first, $this->keys_first_min);
         /* For each package */
         foreach ($struct['Packages'] as $key => $value) {
             /* Analysis of the structure to each package */
-            $this->check_field('Packages', $value, $this->keys_package, array());
+            $this->check_field(array('Root','Packages', $key), $value, $this->keys_package, array());
             /* ----- TYPE ----- */
             /* If the type of package is unknown */
             if (!in_array($value['Type'], $this->keys_type)) {
-                $this->logger->error($this->getApplication()->translator->trans('check.package', array('%val%' => $value['Type'])));
+                $this->logger->error($this->getApplication()->translator->trans('check.package', array('%val%' => $value['Type'], '%path%' => "Root -> Packages -> $key -> Type")));
 
                 exit(-1);
             }
             /* ----- FILES ----- */
-						/* For each file */
-						$this->check_files($value['Files']);
+			/* For each file */
+			$this->check_files(array('Root','Packages', $key, 'Files'), $value['Files']);
             /* ----- BUILD and RUNTIME ----- */
             /* Analysis of the "build" structure */
             if (isset($value['Build'])) {
-                $this->check_field('Build', $value['Build'], array('Dependencies', 'Commands'), array());
+                $this->check_field(array('Root','Packages', $key, 'Build'), $value['Build'], array('Dependencies', 'Commands'), array());
             }
             /* Analysis of the "run" structure */
             if (isset($value['Runtime'])) {
-                $this->check_field('Runtime', $value['Runtime'], array('Dependencies'), array('Dependencies'));
+                $this->check_field(array('Root','Packages', $key, 'Runtime'), $value['Runtime'], array('Dependencies'), array('Dependencies'));
             }
             $key_dependencies = array('Build', 'Runtime','Test');
             /* For the "build" and "run" and "test" dependencies */
@@ -155,14 +160,14 @@ class Check extends Command
 			    foreach ($value[$key_dependencies[$i]]['Dependencies'] as $d_key => $d_value) {
 				    /* If the dependency is empty */
 				    if (empty($d_value)) {
-					    $this->logger->error($this->getApplication()->translator->trans('check.void', array('%key%' => $d_key)));
+					    $this->logger->error($this->getApplication()->translator->trans('check.void', array('%key%' => $d_key, '%path%' => "Root -> Packages -> $key -> $key_dependencies[$i] -> Dependencies -> $d_key")));
 
 					    exit(-1);
 				    }
 				    /* If the dependency is different depending on the distributions */
 				    if (is_array($d_value)) {
 					    /* Analysis of the "dependency" structure (where it has to have names of the distributions) */
-					    $this->check_field($d_key, $d_value, $this->key_dist, array());
+					    $this->check_field(array('Root','Packages', $key, $key_dependencies[$i], 'Dependencies', $d_key), $d_value, $this->key_dist, array());
 					    /* For each name of ditribution */
 					    foreach ($d_value as $v_key => $v_value) {
 						    /* Analysis of the "specific distribution dependency" structure (where it has to
@@ -170,19 +175,21 @@ class Check extends Command
 						     /* if the distibution dont't have any dependencies */
 						     if(!is_array($v_value)) {
 						     	if($v_value != "<none>") {
-						     		 $this->logger->error($this->getApplication()->translator->trans('check.incorrect', array('%field%' => $v_key, '%value%' => $v_value)));
+									$field = implode(' -> ', array('Root', 'Packages', $key, $key_dependencies[$i], 'Dependencies', $d_key, $v_key, $v_value));
+						     		$this->logger->error($this->getApplication()->translator->trans('check.incorrect', array('%field%' => $v_key, '%value%' => $v_value, '%path%' => $field)));
 						     		 exit(-1);
 						     		
 						     	}
 						     }
 						     else {
-						    $this->check_field($v_key, $v_value, $this->versions[$v_key], array('All'));
+								 $this->check_field(array('Root','Packages', $key, $key_dependencies[$i], 'Dependencies', $d_key, $v_key), $v_value, $this->versions[$v_key], array('All'));
 					    		}
 					    	
 					    }
 				    } else { /* If the dependency is the same for all distributions */
 					    if ($d_value != '*') {
-						    $this->logger->error($this->getApplication()->translator->trans('check.incorrect', array('%field%' => $d_key, '%value%' => $d_value)));
+							$field = implode(' -> ', array('Root', 'Packages', $key, $key_dependencies[$i], 'Dependencies', $d_key, $d_value));
+							$this->logger->error($this->getApplication()->translator->trans('check.incorrect', array('%field%' => $d_key, '%value%' => $d_value, '%path%' => $field)));
 
 						    exit(-1);
 					    }
@@ -193,14 +200,14 @@ class Check extends Command
             /* ----- INSTALL ----- */
             /* Analysis of the "install" structure */
             if (isset($value['Install'])) {
-                $this->check_field('Install', $value['Install'], array('Pre', 'Post'), array());
+                $this->check_field(array('Root','Packages', $key, 'Install'), $value['Install'], array('Pre', 'Post'), array());
             }
             /* ----- TEST ----- */
             if (isset($value['Test'])) {
-                $this->check_field('Test', $value['Test'], array('Files','Dependencies', 'Commands'), array());
+                $this->check_field(array('Root','Packages', $key, 'Test'), $value['Test'], array('Files','Dependencies', 'Commands'), array());
 								if (isset($value['Test']['Files'])) {
 									/* For each test file */
-									$this->check_files($value['Test']['Files']);
+									$this->check_files(array('Root','Packages', $key, 'Test', 'Files'), $value['Test']['Files']);
 								}
             }
         }
