a9c1b6e9b53678f2e811c2a81cd5a1c5f7936602
Damien MEHALA <damien.mehala@me.com>
2016-03-12 05:31:15 -0600

    Add support of docker deamon

--- paquito.sh
+++ paquito.sh
@@ -12,4 +12,23 @@ if [[ ! $modules =~ "posix" ]] ; then
 		m_posix='-d extension=posix.so'	
 fi
 
+# Launch docker service
+# We suppose docker is installed 
+# Perform some very rudimentary platform detection and launch docker service
+# TODO: What happen' if lsb_release do not exist ?
+if lsb_release -v "$@" > /dev/null 2>&1; then
+        lsb_dist="$(lsb_release -si | tr '[:upper:]' '[:lower:]')"
+
+        case "$lsb_dist" in
+            debian|centos)
+                    sudo service docker start;;
+                    
+            arch)
+                    sudo systemctl start docker;;
+                    
+            *)
+                    echo -e "Veillez a démarrer le deamon Docker avant d\'exécuter Paquito\nConsultez: https://docs.docker.com/engine/installation/linux/\n\n";;
+        esac
+fi
+
 php -d open_basedir= $m_phar $m_posix /usr/share/paquito.phar $*
